09780cbee0d4b58cc28034053b92769475e23e0e
otomay <54224340+otomay@users.noreply.github.com>
2022-08-16 13:08:16 -0300

    Update modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -26,12 +26,9 @@ dont_starve_compatible = false
 reign_of_giants_compatible = false
 all_clients_require_mod = true
 
-
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
-
 server_filter_tags = {}
-local string = ""
 
 configuration_options =
 {
