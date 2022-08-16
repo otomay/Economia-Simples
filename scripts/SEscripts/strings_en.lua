00d10a705eb737ae0915a5e23e60c6cc691fff79
otomay <54224340+otomay@users.noreply.github.com>
2022-08-16 12:07:20 -0300

    Create strings_en.lua

---
+++ scripts/SEscripts/strings_en.lua
@@ -0,0 +1,47 @@
+STRINGS.NAMES.DEER_ANTLER3 = "Deer Antler"
+STRINGS.NAMES.SUCCULENT_PICKED = STRINGS.NAMES.SUCCULENT_PICKED or "Succulent"
+STRINGS.NAMES.WINTER_ORNAMENT_LIGHT1 = STRINGS.NAMES.WINTER_ORNAMENT_LIGHT1 or "Red Christmas Orb"
+STRINGS.NAMES.WINTER_ORNAMENT_LIGHT2 = STRINGS.NAMES.WINTER_ORNAMENT_LIGHT2 or "Green Christmas Orb"
+STRINGS.NAMES.WINTER_ORNAMENT_LIGHT3 = STRINGS.NAMES.WINTER_ORNAMENT_LIGHT3 or "Blue Christmas Orb"
+STRINGS.NAMES.WINTER_ORNAMENT_LIGHT4 = STRINGS.NAMES.WINTER_ORNAMENT_LIGHT4 or "White Christmas Orb"
+
+STRINGS.NAMES.SECOIN = "Simple Gold"
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.LUCKAMULET = "Simple Gold"
+
+STRINGS.NAMES.STEALER = "Gold Digger"
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.STEALER = "Hardy, hearty and multi-purpose"
+
+STRINGS.NAMES.GOLDSTAFF = "Midas Wand"
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.GOLDSTAFF = "Turns everything into gold"
+
+STRINGS.NAMES.VIPCARD = "VIP Card"
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.VIPCARD = "For the shopaholics"
+
+STRINGS.NAMES.LUCKAMULET = "Lucky Charm"
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.LUCKAMULET = "Blessings crystalized into a gem"
+
+STRINGS.NAMES.COIN_BUNDLE = "a bag of coins"
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.COIN_BUNDLE = "coins:"
+
+STRINGS.SIMPLEECONOMY={
+[1] = "Let’s begin our journey of gold farming~",
+[2] = "Trade",
+[3] = "Instructions",
+[4] = "Back",
+[5] = "Close",
+[6] = "Next",
+[7] = "Previous",
+[8] = "Random Blueprint",
+[9] = "+ ",
+[10] = "+ ",
+[11] = "Those items could not be traded in this market.",
+[12] = "There is nothing that can be disintegrated.",
+[13] = "Shopping level increased!",
+[14] = "Psst... I have ",
+[15] = " coins.",
+[16] = "Holy cow, I have ",
+[17] = " coins.",
+[18] = " coins.",
+[19] = " coins.",
+[20] = "Refresh"
+}
