df5a7ebf284c93d58c8d059ff8738386229075e8
otomay <54224340+otomay@users.noreply.github.com>
2022-08-16 10:37:39 -0300

    Tradução e edição da descrição

--- modinfo.lua
+++ modinfo.lua
@@ -1,17 +1,18 @@
 name =
 [[ Economia Simples ]]
 description =
-[[[h1] Alteracoes [/h1]
+[[[b]Baseado em:[/b] [url=https://steamcommunity.com/sharedfiles/filedetails/?id=2460675139]Simple Economy (Fork of Shaoqun Liu)[/url]
+[h1] Alteracoes [/h1]
 [list]
 [*]Sem a hotkey obrigatoria de abrir a loja [strike](que atrapalhava quando tava digitando)[/strike]
 [*]Corrigido bug que crashava o server quando um monstro era morto no limbo
+[*]Parcialmente traduzido para Portugues
 [/list]
 
-[h1] Referencias e creditos [/h1]
-[b]Edicao em cima de:[/b] [url=https://steamcommunity.com/sharedfiles/filedetails/?id=2460675139]Simple Economy (Fork of Shaoqun Liu)[/url]
-[b]Mod original:[/b] [url=https://steamcommunity.com/sharedfiles/filedetails/?id=1115709310]Simple Economy[/url]
-
-[url=https://github.com/otomay/Economia-Simples]Projeto no github[/url]
+[h1] MOD Original [/h1]
+[url=https://steamcommunity.com/sharedfiles/filedetails/?id=1115709310]Simple Economy[/url]
+[h1] Projeto [/h1]
+[url=https://github.com/otomay/Economia-Simples]Github[/url]
 ]]
 author = "Otomai"
 version = "1.0.1"
@@ -43,56 +44,56 @@ configuration_options =
 {
     {
         name = "Language",
-        label = "Language",
+        label = "Idioma",
         options =   {
-                        {description = "English", data = false},
+                        {description = "Portugues (BR)", data = false},
                         {description = "简体中文", data = true},
                     },
         default = false,
     },
     {
         name = "KillBonus",
-        label = "Reward on killing mobs",
+        label = "Recompensa por matar monstros",
         options =   {
                         {description = "Off", data = false},
-						{description = "100% of mobs max health", data = 1.0},
-						{description = "50% of mobs max health", data = 0.50},
-						{description = "25% of mobs max health", data = 0.25},
-                        {description = "10% of mobs max health", data = 0.1},
-						{description = "8% of mobs max health", data = 0.08},
-						{description = "5% of mobs max health", data = 0.05},
-						{description = "2% of mobs max health", data = 0.02},
-						{description = "1% of mobs max health", data = 0.01},
-						{description = "0.5% of mobs max health", data = 0.005},
-						{description = "0.1% of mobs max health", data = 0.001},
+						{description = "100% da vida maxima do monstro", data = 1.0},
+						{description = "50% da vida maxima do monstro", data = 0.50},
+						{description = "25% da vida maxima do monstro", data = 0.25},
+                        			{description = "10% da vida maxima do monstro", data = 0.1},
+						{description = "8% da vida maxima do monstro", data = 0.08},
+						{description = "5% da vida maxima do monstro", data = 0.05},
+						{description = "2% da vida maxima do monstro", data = 0.02},
+						{description = "1% da vida maxima do monstro", data = 0.01},
+						{description = "0.5% da vida maxima do monstro", data = 0.005},
+						{description = "0.1% da vida maxima do monstro", data = 0.001},
                     },
         default = 0.02,
     },
     {
         name = "Disintegrate",
-        label = "Allow disintegration",
+        label = "Permitir desintegracao",
         options =   {
-                        {description = "NO", data = false, hover = "Not allow to use Midas Wand to disintegrate items.".."\n".."点金杖仅可用来击杀低血量怪物以换取金币".."\n".."选此选项后以点金杖攻击低血量生物可100%几率击杀"},
-                        {description = "YES", data = true, hover = "Allow to use Midas Wand to disintegration items.".."\n".."选此选项后以点金杖攻击低血量生物仅可25%几率击杀"},
+                        {description = "NAO", data = false, hover = "Nao permite que a varinha de midas desintegre itens"},
+                        {description = "SIM", data = true, hover = "Permite que a varinha de midas desintegre itens"},
                     },
         default = false,
     },
     {
         name = "Precious_Disintegratable",
-        label = "Allow disintegrating precious items",
+        label = "Permitir desintegracao de itens preciosos (e nao unicos, como olho de deerclops e dente do walrus)",
         options =   {
-                       {description = "YES", data = true, hover = "Allow to distintegrate items in the list of precious items."},
-                       {description = "NO", data = false, hover ="Not allow to distintegrate items in the list of precious items."},
+                       {description = "SIM", data = true, hover = "Permite desintegrar itens da lista de preciosos"},
+                       {description = "NAO", data = false, hover ="Não permite desintegrar itens da lista de preciosos"},
                     },
         default = false,
     },
     {
         name = "CanDragMainbutton",
-        label = "CanDragMainbutton",
-        hover = "the Mainbutton can be dragged",
+        label = "Arrastar botao da loja",
+        hover = "Permite os jogadores arrastarem e posicionarem o botao livremente, de acordo com suas preferencias",
         options = {
-            {description = "YES", data = true, hover = "the Mainbutton can be dragged"},
-            {description = "NO", data = false, hover = "the Mainbutton can't be dragged"},
+            {description = "SIM", data = true, hover = "O botao pode ser arrastado"},
+            {description = "NAO", data = false, hover = "O botao nao pode ser arrastado"},
         },
         default = true,
     },
